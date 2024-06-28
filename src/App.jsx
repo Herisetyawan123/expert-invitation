020e6b5a06f671b65c3693f4ecc33c0644ee64cc
Heri Setyawan <herisetyawan233@gmail.com>
2024-06-28 07:21:46 +0700

    feat: cover png

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import CoverPage from "./section/cover-page";
+
 function App() {
-  return <></>;
+  return (
+    <main className="w-[100vw] overflow-x-hidden">
+      <CoverPage />
+      <h1>haloo dunia</h1>
+    </main>
+  );
 }
 
 export default App;
